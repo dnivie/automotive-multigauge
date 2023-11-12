34315b4d6c49977c090e28ad717b024569e14484
Eivind <eivindd247@gmail.com>
2023-11-12 17:37:59 +0100

    fixed private/publics in graphics class

--- multigauge/Graphics.h
+++ multigauge/Graphics.h
@@ -10,25 +10,21 @@ class Graphics
         int16_t getSensorHistory(uint8_t index);
         void addSensorHistory(int16_t val);
         void init();
-
-        //const uint8_t sensorHistoryLength = 128;
-        //uint8_t sensorHistory[sensorHistoryLength];
-        //uint8_t sensorHistoryPos = sensorHistoryLength - 1;
-    
-        void drawGraph(uint8_t x, uint8_t y, uint8_t len, uint8_t height);
-        void drawHorizontalDotLine(uint8_t x, uint8_t y, uint8_t len);
         uint16_t getBoostMax();
         int16_t getBoostMin();
         void resetBoostMax();
-        void drawGauge(uint8_t x, uint8_t y, uint8_t len, uint8_t maxHeight, uint16_t boostPressure);
-        void drawAfrGraphics(uint8_t y, uint8_t height, float afr);
-        void drawVerticalBar(uint8_t x, uint8_t y, uint8_t width, uint8_t maxHeight, uint16_t val);
-        void drawBarGraph(int x, int y, int len, int height, int val);
-
         void screenMode0(); // startup screen
         void screenMode1(float val0, float val1); // curved line graphics
         void screenMode2(float val0, float val1); // horizontal line graphics
         void screenMode3(float val0, float val1); // plotted boost curve
+    
+    private:
+        void drawGraph(uint8_t x, uint8_t y, uint8_t len, uint8_t height);
+        void drawHorizontalDotLine(uint8_t x, uint8_t y, uint8_t len);
+        void drawGauge(uint8_t x, uint8_t y, uint8_t len, uint8_t maxHeight, uint16_t boostPressure);
+        void drawAfrGraphics(uint8_t y, uint8_t height, float afr);
+        void drawVerticalBar(uint8_t x, uint8_t y, uint8_t width, uint8_t maxHeight, uint16_t val);
+        void drawBarGraph(int x, int y, int len, int height, int val);
 };
 
 #endif
